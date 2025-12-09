bfd9304732fcef1a6d830b3453f91c625ac1a465
kanhaiya41 <161762630+kanhaiya41@users.noreply.github.com>
2025-12-10 00:06:58 +0530

    first commit

---
+++ backend/index.js
@@ -0,0 +1,37 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import dbConnect from './utills/dbConnect.js';
+import cors from 'cors'
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors(
+    {
+        origin: process.env.URI,
+        credentials: true
+    }
+));
+
+app.use(express.json());
+// app.use(cookieParser());
+
+
+dbConnect();
+
+app.get('/', async (req, res) => {
+    res.send('all runs');
+})
+
+
+
+// app.use(express.static(path.join(__dirname, 'frontend', 'build')));
+
+// app.get('/*splat', (req, res) => {
+//     res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+// });
+
+
+
+app.listen(process.env.PORT, '0.0.0.0', () => console.log(`Server successfully runs on port ${process.env.PORT}`))
