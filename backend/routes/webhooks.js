4203730a678fba8846c1952d95bf56ef12fad137
akashap2304 <122444099+akashap2304@users.noreply.github.com>
2025-12-22 23:05:53 +0530

    akash1

--- backend/routes/webhooks.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { metaEvents, reciveWebhook } from '../controller/webhooks.js';
-const router = express.Router();
-
-
-router.get("/meta", reciveWebhook)
-router.post("/meta", metaEvents);
-
-export default router
